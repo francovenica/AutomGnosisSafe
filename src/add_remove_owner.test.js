d3e8f293dee7b8f930c1ee9e9cb8f9ac5389811a
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2021-03-24 13:47:48 +0100

    Fix linting issues

--- src/add_remove_owner.test.js
+++ src/add_remove_owner.test.js
@@ -42,35 +42,35 @@ describe('Adding and removing owners', () => {
     console.log('Filling Form and submitting tx')
     const existing_owner_hash = accountsSelectors.testAccountsHash.acc1
     try {
-      await clickByText("span", "settings", gnosisPage)
+      await clickByText('span', 'settings', gnosisPage)
       await clickElement(settingsPage.owners_tab, gnosisPage)
       await clickElement(settingsPage.add_owner_btn, gnosisPage)
-      
+
       await clickElement(settingsPage.add_owner_next_btn, gnosisPage)
       await assertElementPresent(errorMsg.error(errorMsg.required), gnosisPage) // asserts error "required" in name
       await clickAndType(settingsPage.add_owner_name_input, gnosisPage, new_owner_name)
-      
+
       await clickElement(settingsPage.add_owner_next_btn, gnosisPage)
       await assertElementPresent(errorMsg.error(errorMsg.required), gnosisPage) // asserts error "required" in name
 
       await clickAndType(settingsPage.add_owner_address_input, gnosisPage, '0xInvalidHash')
       await assertElementPresent(errorMsg.error(errorMsg.valid_ENS_name), gnosisPage)
-      await clearInput("[data-testid='add-owner-address-testid']", gnosisPage, "css")
+      await clearInput("[data-testid='add-owner-address-testid']", gnosisPage, 'css')
 
       await clickAndType(settingsPage.add_owner_address_input, gnosisPage, existing_owner_hash)
       await assertElementPresent(errorMsg.error(errorMsg.duplicated_address), gnosisPage)
-      await clearInput("[data-testid='add-owner-address-testid']", gnosisPage, "css")
+      await clearInput("[data-testid='add-owner-address-testid']", gnosisPage, 'css')
 
       await clickAndType(settingsPage.add_owner_address_input, gnosisPage, new_owner_address)
       await clickElement(settingsPage.add_owner_next_btn, gnosisPage)
 
       await clickElement(settingsPage.add_owner_review_btn, gnosisPage)
-      await assertTextPresent("//div[13]/div[2]/div/p", new_owner_name, gnosisPage)
-      await assertTextPresent("//div[13]/div[2]/div/div/p", new_owner_address, gnosisPage)
-      //Checking the new owner name and address is present in the review step ^^^. Do we need an Id for this?
-      await assertElementPresent("[data-testid='add-owner-submit-btn']", gnosisPage, "css")
+      await assertTextPresent('//div[13]/div[2]/div/p', new_owner_name, gnosisPage)
+      await assertTextPresent('//div[13]/div[2]/div/div/p', new_owner_address, gnosisPage)
+      // Checking the new owner name and address is present in the review step ^^^. Do we need an Id for this?
+      await assertElementPresent("[data-testid='add-owner-submit-btn']", gnosisPage, 'css')
       await gnosisPage.waitForFunction(() => !document.querySelector("[data-testid='add-owner-submit-btn'][disabled]"))
-      //The submit button starts disabled. I wait for it to become enabled ^^^
+      // The submit button starts disabled. I wait for it to become enabled ^^^
       await clickElement(settingsPage.add_owner_submit_btn, gnosisPage)
       await gnosisPage.waitForTimeout(4000)
       await metamask.sign()
@@ -108,38 +108,38 @@ describe('Adding and removing owners', () => {
       done(error)
     }
   }, 180000)
-  test('Deleting owner form filling and tx creation', async(done)=>{
+  test('Deleting owner form filling and tx creation', async (done) => {
     console.log('Deleting owner form filling and tx creation')
     try {
       await gnosisPage.bringToFront()
-      await assertTextPresent("//h5", "Queue transactions will appear here", gnosisPage) 
-      //we need a better way to know if the Queue tab is empty ^^^
-      await clickByText("span", "settings", gnosisPage)
+      await assertTextPresent('//h5', 'Queue transactions will appear here', gnosisPage)
+      // we need a better way to know if the Queue tab is empty ^^^
+      await clickByText('span', 'settings', gnosisPage)
       await clickElement(settingsPage.owners_tab, gnosisPage)
       await assertElementPresent("[data-testid='remove-owner-btn']", gnosisPage, 'css')
-      
-      var count = 0
-      var aux = 1
-      await gnosisPage.evaluate( (count, aux)=>{
-        console.log('Entre. Count = ', count, "   aux = ", aux)
-        document.querySelectorAll("[data-testid='owners-row'] span").forEach(x => { 
-          if ( x.innerText === "0xc8b99Dc2414fAA46E195a8f3EC69DD222EF1744F") 
-          aux = 0; 
-        count += aux;
-      })
-      document.querySelectorAll("[data-testid='remove-owner-btn']")[(count === 0) ? 0 : count-1].click()
+
+      let count = 0
+      let aux = 1
+      await gnosisPage.evaluate((count, aux) => {
+        console.log('Entre. Count = ', count, '   aux = ', aux)
+        document.querySelectorAll("[data-testid='owners-row'] span").forEach(x => {
+          if (x.innerText === '0xc8b99Dc2414fAA46E195a8f3EC69DD222EF1744F')
+            aux = 0;
+          count += aux;
+        })
+        document.querySelectorAll("[data-testid='remove-owner-btn']")[(count === 0) ? 0 : count - 1].click()
       }, count, aux)
-      //The only way I could find the correct "remove owner" icon to click ^^^
+      // The only way I could find the correct "remove owner" icon to click ^^^
       await clickElement(settingsPage.remove_owner_next_btn, gnosisPage)
-      await openDropdown({selector: '[id="mui-component-select-threshold"]', type:'css'}, gnosisPage)
-      await clickElement({selector: "[data-value='2']", type:'css'}, gnosisPage)
+      await openDropdown({ selector: '[id="mui-component-select-threshold"]', type: 'css' }, gnosisPage)
+      await clickElement({ selector: "[data-value='2']", type: 'css' }, gnosisPage)
       await gnosisPage.waitForTimeout(2000)
       await clickElement(settingsPage.remove_owner_review_btn, gnosisPage)
-      await assertTextPresent("//div[13]/div[2]/div/p", new_owner_name, gnosisPage)
-      await assertTextPresent("//div[13]/div[2]/div/div/p", new_owner_address, gnosisPage)
-      await assertElementPresent("[data-testid='remove-owner-review-btn']", gnosisPage, "css")
+      await assertTextPresent('//div[13]/div[2]/div/p', new_owner_name, gnosisPage)
+      await assertTextPresent('//div[13]/div[2]/div/div/p', new_owner_address, gnosisPage)
+      await assertElementPresent("[data-testid='remove-owner-review-btn']", gnosisPage, 'css')
       await gnosisPage.waitForFunction(() => !document.querySelector("[data-testid='remove-owner-review-btn'][disabled]"))
-      await clickElement(settingsPage.remove_owner_submit_btn, gnosisPage) 
+      await clickElement(settingsPage.remove_owner_submit_btn, gnosisPage)
       await gnosisPage.waitForTimeout(4000)
       await metamask.sign()
       done()
@@ -176,20 +176,19 @@ describe('Adding and removing owners', () => {
       done(error)
     }
   }, 180000)
-  test("Verifying owner deletion", async(done) => {
-  console.log("Verifying owner deletion")
-  try{
-    await assertTextPresent("//h5", "Queue transactions will appear here", gnosisPage) 
-    await clickByText('button > span > p', 'History', gnosisPage)
-    const nonce = await gFunc.getNumberInString(transactionsTab.tx_nonce, gnosisPage, 'css')
-    expect(nonce).toBe(current_nonce)
-    const executed_tx_status = await getInnerText(transactionsTab.tx2_status, gnosisPage, 'css')
-    expect(executed_tx_status).toBe("Success")
-  done()
-  }
-  catch (error){
-  console.log(error)
-  done()
-  }
+  test('Verifying owner deletion', async (done) => {
+    console.log('Verifying owner deletion')
+    try {
+      await assertTextPresent('//h5', 'Queue transactions will appear here', gnosisPage)
+      await clickByText('button > span > p', 'History', gnosisPage)
+      const nonce = await gFunc.getNumberInString(transactionsTab.tx_nonce, gnosisPage, 'css')
+      expect(nonce).toBe(current_nonce)
+      const executedTxStatus = await getInnerText(transactionsTab.tx2_status, gnosisPage, 'css')
+      expect(executedTxStatus).toBe('Success')
+      done()
+    } catch (error) {
+      console.log(error)
+      done()
+    }
   }, 120000)
 })
